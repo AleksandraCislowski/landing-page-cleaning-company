b3e8619f60f5d74bfa3bb5bf50fc2c736ad454ef
aleksandra <cislowski.aleksandra@gmail.com>
2026-03-18 19:55:19 +0100

    i18n + first translations

---
+++ src/i18n/config.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import en from '../locales/en.json';
+import sv from '../locales/sv.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: { translation: en },
+      sv: { translation: sv },
+    },
+  });
+
+export default i18n;
