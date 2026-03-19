749caf45890e992ff1f3aea7a22b30e9a8e994e8
aleksandra <cislowski.aleksandra@gmail.com>
2026-03-19 16:54:47 +0100

    year counter component

---
+++ src/components/YearCounter.tsx
@@ -0,0 +1,9 @@
+export function FirmaYearCounter() {
+  const getFirmaYear = new Date().getFullYear() - 2019;
+  return getFirmaYear;
+}
+
+export function PersonYearCounter() {
+  const getPersonYear = new Date().getFullYear() - 2016;
+  return getPersonYear;
+}
