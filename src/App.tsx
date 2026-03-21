e699ea93574fa4694c005bf4c8fad311b6d6134a
aleksandra <cislowski.aleksandra@gmail.com>
2026-03-21 19:28:43 +0100

    navigation to photo gallery always to the top of the subpage

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { useEffect } from 'react';
 function ScrollToTop() {
   const { pathname } = useLocation();
   useEffect(() => {
-    window.scrollTo(0, 0);
+    if (pathname !== '/') {
+      window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+    }
   }, [pathname]);
   return null;
 }
